71d15815075839ac7ab5ab6ee5d2fee9f2afb914
codedbychavez <codedbychavez@gmail.com>
2022-11-09 13:09:45 -0300

    Version 1.1

--- src/components/Downloads/Modal.vue
+++ src/components/Downloads/Modal.vue
@@ -2,9 +2,7 @@
   <div class="modal" :class="[canShowModal ? 'show' : 'hide']">
     <div v-for="(item, index) in items" :key="index" class="modal-flex-row">
       <div class="modal-icon-col">
-        <a href="#" target="_blank">
-          <img src="@/assets/images/icon.svg" />
-        </a>
+        <Icon class="modal-icon" />
       </div>
       <div class="modal-text-col">
         <p class="modal-text">{{ item.title }}</p>
@@ -14,10 +12,14 @@
 </template>
 
 <script>
+import Icon from "../Icons/Icon.vue";
 export default {
   props: {
     canShowModal: Boolean,
     items: Array,
+  },
+  components: {
+    Icon,
   },
 };
 </script>
@@ -35,7 +37,7 @@ export default {
   width: 280px;
   box-shadow: 0px 20px 24px -4px rgba(16, 24, 40, 0.08),
     0px 8px 8px -4px rgba(16, 24, 40, 0.03);
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   border-radius: 5px;
 }
 
@@ -47,6 +49,13 @@ export default {
   display: flex;
   height: 56px;
   border-radius: 5px;
+  // NB: Primary Red
+  &:hover {
+    color: #ff1f27;
+    .modal-icon {
+      stroke: #ff1f27;
+    }
+  }
 }
 
 .modal-icon-col,
@@ -60,7 +69,13 @@ export default {
 
 .modal-icon-col {
   width: 56px;
+  // NB: Transparencies/Grey Light 50%
   border-right: 1px solid #d8d9db80;
+}
+
+.modal-icon {
+  // NB: Primary Blue
+  stroke: #1b1464;
 }
 
 .modal-text-col {
